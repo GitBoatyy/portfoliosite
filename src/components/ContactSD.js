d801658fb5f040a4a161d3bc361b022c20fb7014
Ben Coulthard <bcou94@gmail.com>
2022-10-26 20:04:00 -0700

    added form script to contactSD for netlify form sumbit

--- src/components/ContactSD.js
+++ src/components/ContactSD.js
@@ -1,9 +1,32 @@
 import React from "react";
 export default function ContactSD(){
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data)
+          .map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+          )
+          .join("&");
+      }
+    
+      function handleSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", name, email, message }),
+        })
+          .then(() => alert("Message sent!"))
+          .catch((error) => alert(error));
+      }
     return(
         <section id="contactSD">
             <div className="containercontact">
-                <form netlify name="contact" className="contactform" >
+                <form netlify name="contact" className="contactform" onSubmit={handleSubmit}>
+
                     <h2 className="grayf">
                         Contact Me
                     </h2>
@@ -11,13 +34,13 @@ export default function ContactSD(){
                     If you have an opportunity to share, or would like to discuss anything, you can leave your details below.                    </p>
                         <div className="formcontainer ">
                             <div className="contactinput">
-                                <input className="contactinput" type="text" id="name" name="name" placeholder="Name"/>
+                                <input onChange={(e) => setName(e.target.value)} className="contactinput" type="text" id="name" name="name" placeholder="Name"/>
                             </div>
                             <div className="contactinput">
-                                <input className="contactinput" type="email" id="email" name="email" placeholder="Email" />
+                                <input onChange={(e) => setEmail(e.target.value)} className="contactinput" type="email" id="email" name="email" placeholder="Email" />
                             </div>
                             <div className="contactinput">
-                                <textarea className="contactinput" id="message" name="message" placeholder="Message"/>
+                                <textarea onChange={(e) => setMessage(e.target.value)} className="contactinput" id="message" name="message" placeholder="Message"/>
                             </div>
                             <button type="submit">
                                 Submit
