3198b4cc6fdf4e78b9f1d82023097bc7db8a5a1e
Ben Coulthard <bcou94@gmail.com>
2022-10-30 17:41:30 -0700

    added weather app the projectSD. updated image for mediasearch. updated project descriptions

--- src/components/ProjectsSD.jsx
+++ src/components/ProjectsSD.jsx
@@ -25,13 +25,13 @@ export default function ProjectsSD() {
               built entirely in vanilla JS, with hand coded CSS to match the
               Boeing OMF system exactly. Used by Aircraft maintenance personnel
               during training to familiarize themselves with navigating the
-              system.
+              system. Pages are being added daily to fill out the simulation.
             </div>
           </div>
           <div id="project2" className="projects">
             <div className="projectdescription">
-              A Media search website built with React.js and utilizing OMDb API
-              to pull Poster, Title, Type of media, and Year made
+              A media search website built with React.js and utilizing OMDb API
+              to pull and display media information.
             </div>
             <div className="projectimage">
               <a
@@ -40,10 +40,31 @@ export default function ProjectsSD() {
                 rel="noopener noreferrer"
               >
                 <img
-                  src="https://i.imgur.com/8Yf5uNe.png"
+                  src="https://i.imgur.com/4R96XUX.png"
                   alt="MediaSearch website - built with React and utilizing OMDb API"
                 />
               </a>
+            </div>
+          </div>
+          <div id="project3" className="projects">
+            <div className="projectimage">
+              <a
+                href="https://boatweather.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://i.imgur.com/q1awlmG.png"
+                  alt="Weather app using openweathermap api and unsplash api. click for live site."
+                />
+              </a>
+            </div>
+            <div className="projectdescription">
+              A Weather app coded in vanilla JS, pulling data from
+              openweathermap API and sourcing local background images from
+              unsplashed API. Uses localstorage to save where you left off. I am
+              still working on fading between the location images for a smoother
+              transition.
             </div>
           </div>
         </div>
